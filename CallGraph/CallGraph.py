b66b57445f46b5e3169a7f889af76333f0036075
kaixa <690750353@qq.com>
2020-05-27 19:56:06 +0800

    添加命令行选项d，使用新的绘图库绘制调用图。 Add a new cli option '--drawer/-d' to specify modules to draw call graph.

--- CallGraph/CallGraph.py
+++ CallGraph/CallGraph.py
@@ -13,7 +13,10 @@ parser.add_argument('--enable-stl', '-stl', dest='stl', type=bool, default=False
                     help='Draw Call Graphs of C++ Standard Library.')
 parser.add_argument('--enable-plt', '-plt', dest='plt', type=bool, default=False, required=False, nargs='?',
                     help='Draw Call Graphs of External Functions like puts and printf in libc.')
-parser.add_argument('--file', '-f', dest='filename.s', metavar='entry file',required=True)
+parser.add_argument('--file', '-f', dest='file', metavar='entry file', required=True,
+                    help='Assembly file for callgraph to draw.')
+parser.add_argument('--drawer', '-d', dest='drawer', default='pygraphviz', choices=['pygraphviz', 'igraph'],
+                    help='Specify a depicter to draw call graph. By default, callgraph uses pygraphviz.')
 args = parser.parse_args()
 
 
@@ -42,7 +45,8 @@ def Main():
     global assembleCode
     PLTFlag = args.plt is not False
     STLFlag = args.stl is not False
-    fileName:str = args.file
+    print(args)
+    fileName: str = args.file
     if not fileName.lower().endswith('.s'):
         Help()
         exit(1)
@@ -50,7 +54,7 @@ def Main():
     Build()
     name = fileName[:-2]
     Generation(name + ".json")
-    DrawPic(name + ".json", name + ".png", PLTFlag, STLFlag)
+    DrawPic(name + ".json", name + ".png", PLTFlag, STLFlag, args.drawer)
 
 
 def Help():
--- CallGraph/CallGraphDepicter.py
+++ CallGraph/CallGraphDepicter.py
@@ -1,12 +1,19 @@
 import json
-import pygraphviz as pyg
 from CallGraph.utils.Queue import *
 from CallGraph.utils import getOriginFuncName, isPLT, isNotSTL
 
 
-def Drawer(entryPoint, symbolTable, bondMap, picName, DrawPLTFlag, DrawSTLFlag):
-    g = pyg.AGraph()
-    g.add_node(getOriginFuncName(symbolTable[entryPoint]))
+def Drawer(entryPoint, symbolTable, bondMap, picName, DrawPLTFlag, DrawSTLFlag, drawer):
+    symbolCount = len(symbolTable)
+    if drawer == 'igraph':
+        import igraph
+        g = igraph.Graph(directed=True)
+        g.add_vertices(symbolCount)
+        g.vs['name'] = [getOriginFuncName(s) for s in symbolTable]
+    else:
+        import pygraphviz as pyg
+        g = pyg.AGraph()
+        g.add_node(getOriginFuncName(symbolTable[entryPoint]))
     visited = {}
     q = queue()
     q.push(entryPoint)
@@ -23,27 +30,43 @@ def Drawer(entryPoint, symbolTable, bondMap, picName, DrawPLTFlag, DrawSTLFlag):
         for calleeID in bondMap[str(callerID)]:
             if not DrawPLTFlag and isPLT(symbolTable[calleeID]) or not DrawSTLFlag and isNotSTL(symbolTable[calleeID]):
                 continue
-            g.add_edge(getOriginFuncName(symbolTable[callerID]), getOriginFuncName(symbolTable[calleeID]))
+            if drawer == 'igraph':
+                g.add_edge(int(callerID), int(calleeID))
+            else:
+                g.add_edge(getOriginFuncName(symbolTable[callerID]), getOriginFuncName(symbolTable[calleeID]))
             if calleeID not in visited:
                 q.push(calleeID)
-    g.layout(prog='dot')
-    g.draw(picName)
+    if drawer == 'igraph':
+        picSize = 20 * symbolCount
+        vertexColors = ['blue'] * symbolCount
+        vertexColors[entryPoint] = 'red'
+        style = {
+            'vertex_label': g.vs['name'],
+            'bbox': (0, 0, picSize, picSize),
+            'layout': g.layout('lgl'),
+            'vertex_color': vertexColors,
+            'vertex_label_dist': 1
+        }
+        igraph.plot(g, picName, **style)
+    else:
+        g.layout(prog='dot')
+        g.draw(picName)
 
 
-def DrawPic(fileName, picName, DrawPLTFlag, DrawSTLFlag):
+def DrawPic(fileName, picName, DrawPLTFlag, DrawSTLFlag, drawer):
     with open(fileName, 'r') as f:
         jsonObj = json.loads(f.read())
     entryPoint = jsonObj['entryPoint']
     symbolTable = jsonObj['symbolTable']
     bondMap = jsonObj['bondMap']
-    Drawer(entryPoint, symbolTable, bondMap, picName, DrawPLTFlag, DrawSTLFlag)
+    Drawer(entryPoint, symbolTable, bondMap, picName, DrawPLTFlag, DrawSTLFlag, drawer)
 
 
 def Main():
     global assembleCode
     FILENAME = 'result.json'
     PICNAME = 'result.png'
-    DrawPic(FILENAME, PICNAME, False, False)
+    DrawPic(FILENAME, PICNAME, False, False, 'pygraphviz')
 
 
 if __name__ == '__main__':
