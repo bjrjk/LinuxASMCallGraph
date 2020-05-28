dbe8574a69b042b2ebf978ac7a1f7df0c97677ac
Jack Ren <bjrjk@qq.com>
2020-05-28 09:02:21 +0800

    Update setup.py to v1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='LinuxASMCallGraph',
-    version='1.2',
+    version='1.3',
     packages=['CallGraph', 'CallGraph.utils'],
     license='GPL-3.0',
     entry_points={
